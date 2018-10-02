9c0afe90329e0c9c3ff0f58fb7a7a3040dc8314e
PeterTillema <petertillema@solcon.nl>
2018-10-02 23:29:30 +0200

    Update documentation

--- docs/subprograms algorithm.c
+++ docs/subprograms algorithm.c
@@ -1,4 +1,4 @@
-{prog, line} = getProgAndLine(line) {
+prog_t getProgram(line) {
     prog_t prog;
     uint8_t progIndex = amountOfPrograms;
     uint16_t outputLine = prog.endingLine - prog.startingLine;
@@ -7,15 +7,7 @@
         prog = programs[progIndex - 1];
         
         if (line >= prog.startingLine && line <= prog.endingLine) {
-            while (++progIndex != amountOfPrograms) {
-                prog_t prog2 = programs[progIndex];
-                
-                if (prog.depth + 1 == prog2.depth && prog2.startingLine >= prog.startingLine && prog2.endingLine <= prog.endingLIne) {
-                    outputLine -= prog2.endingLine - prog2.startingLine + 1;
-                }
-            }
-            
-            return [prog, line];
+            return prog;
         }
     }
 }
